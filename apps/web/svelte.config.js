98b8c7a2f76486868a096e98313c22a87ceda6a8
Treido <treido.vendor.pasti@pegadaian.co.id>
2026-04-04 12:55:23 +0700

    Use node adapter for web Docker builds

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from "@sveltejs/adapter-vercel";
+import adapterNode from "@sveltejs/adapter-node";
+import adapterVercel from "@sveltejs/adapter-vercel";
+
+const useNodeAdapter = process.env.SVELTEKIT_ADAPTER === "node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: useNodeAdapter ? adapterNode() : adapterVercel()
   }
 };
 
